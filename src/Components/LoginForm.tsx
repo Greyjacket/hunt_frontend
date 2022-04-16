422baec554b664b579888aae96f52cb6752cc7c7
Alexander Schieppati <you@example.com>
2022-04-16 17:49:15 -0400

    logo updated

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,13 +1,13 @@
 import {FormControl, InputLabel, Input, InputAdornment} from '@mui/material';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import Password from '@mui/icons-material/Password';
-
+import logo from './Images/logo.png';
 
 function LoginForm() {
   return (
     <div className="loginForm">
       <div className='loginLogoHeader'>
-        <img src="Images/logo.png" alt="" />
+        <img src={logo} alt="" />
       </div>
       <FormControl>
         <Input id="username" autoFocus={true} placeholder="Username" startAdornment={
