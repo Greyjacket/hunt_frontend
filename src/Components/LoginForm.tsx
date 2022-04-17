5b6a626d319e2aff41a52b6648827f08d8b9e0af
Alexander Schieppati <asch716@gmail.com>
2022-04-17 16:52:37 -0400

    Loginform logo edited and added

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -2,12 +2,15 @@ import {FormControl, InputLabel, Input, InputAdornment} from '@mui/material';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import Password from '@mui/icons-material/Password';
 import logo from './Images/logo.png';
+import React from 'react';
+
+// import LoadingButton from '@mui/lab/LoadingButton';
 
 function LoginForm() {
   return (
     <div className="loginForm">
       <div className='loginLogoHeader'>
-        <img src={logo} alt="" />
+        <img src={logo} className="loginLogo" alt=""/>
       </div>
       <FormControl>
         <Input id="username" autoFocus={true} placeholder="Username" startAdornment={
