77418acfdd3c30121714ed4c6c545d1cd2561dd4
Greyjacket <asch716@gmail.com>
2022-05-31 21:49:14 -0400

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import './App.css';
 import LoginForm from './Components/LoginForm';
+import Dashboard from './Components/Dashboard';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './pages/ProtectedRoute';
+import { useState } from 'react';
+
+interface LoginState {
+  authenticating: boolean,
+  failure: boolean,
+  userName: String,
+  password: String
+}
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
-    <div className="App">
-      <div className='loginFormContainer'>
-      <LoginForm/>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<div className="App">
+          <div className='loginFormContainer'>
+          <LoginForm/>
+          </div>
+        </div>}>
+      </Route>
+      <Route
+            path='dashboard'
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/Components/Dashboard.tsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+  return (
+    <section className='section'>
+      <h4>Hello, Alex</h4>
+    </section>
+  );
+};
+export default Dashboard;
